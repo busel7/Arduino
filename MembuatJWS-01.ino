487e8f0beab6549e1eb70a2a6ab58f222f4e6cd5
Bonny Useful <bonny.useful@gmail.com>
2019-10-18 00:35:26 +0700

    Add files via upload

---
+++ MembuatJWS-01.ino
@@ -0,0 +1,109 @@
+/*
+ * JADWAL WAKTU SHOLAT MENGGUNAKAN NODEMCU ESP8266, LED P10, RTC DS3231, BUZZER
+ * FITUR :  JADWAL SHOLAT 5 WAKTU DAN TANBIH IMSAK, JAM BESAR, TANGGAL, SUHU, ALARAM ADZAN DAN TANBIH IMSAK,
+ *          DAN HITUNG MUNDUR IQOMAH DAN UBAH WAKTU LEWAT WIFI DENGAN BROWSER.
+ * 
+
+Pin on  DMD P10     GPIO      NODEMCU               Pin on  DS3231      NODEMCU                   Pin on  Buzzer       NODEMCU
+        2  A        GPIO16    D0                            SCL         D1 (GPIO 5)                       +            RX (GPIO 3)
+        4  B        GPIO12    D6                            SDA         D2 (GPIO 4)                       -            GND
+        8  CLK      GPIO14    D5                            VCC         3V
+        10 SCK      GPIO0     D3                            GND         GND
+        12 R        GPIO13    D7
+        1  NOE      GPIO15    D8
+        3  GND      GND       GND
+
+Catatan : 
+o Perlu Power Eksternal 5V ke LED P10.
+o Saat Flashing (upload program) cabut sementara pin untuk buzzer.
+
+Project Git
+- https://github.com/busel7/Arduino
+
+Eksternal Library
+- HJS589(DMD3 porting for ESP8266 by Ahmad Herman) < DMD3 by Wardi Utari @ MFH Robotic (info https://www.facebook.com/MFH.Robotic/)) < DMD : https://github.com/freetronics/DMD
+- PrayerTime : https://github.com/asmaklad/Arduino-Prayer-Times
+- RTC DS3231 : https://github.com/Makuna/Rtc
+- ArduinoJson V6 : https://github.com/bblanchon/ArduinoJson
+
+Diskusi Grup Untuk1000Masjid : https://www.facebook.com/groups/761058907424496/
+
+www.grobak.net - www.elektronmart.com
+
+Updated : 24 September 2019
+*/
+
+
+#include <HJS589.h>
+
+#include <fonts/ElektronMart6x8.h>
+#include <fonts/ElektronMart6x16.h>
+#include <fonts/ElektronMart5x6.h>
+#include <fonts/ElektronMartArabic6x16.h>
+#include <fonts/ElektronMartArabic5x6.h>
+
+
+//----------------------------
+// SETUP DMD HJS589
+
+#define DISPLAYS_WIDE 2 
+#define DISPLAYS_HIGH 1
+HJS589 Disp(DISPLAYS_WIDE, DISPLAYS_HIGH);  // Jumlah Panel P10 yang digunakan (KOLOM,BARIS)
+
+
+
+//----------------------------------------------------------------------
+// HJS589 P10 FUNGSI TAMBAHAN UNTUK NODEMCU ESP8266
+
+void ICACHE_RAM_ATTR refresh() { 
+  
+  Disp.refresh();
+  timer0_write(ESP.getCycleCount() + 80000);  
+
+}
+
+void Disp_init() {
+  
+  Disp.start();
+  noInterrupts();
+  timer0_isr_init();
+  timer0_attachInterrupt(refresh);
+  timer0_write(ESP.getCycleCount() + 80000);
+  interrupts();
+  Disp.clear();
+  
+}
+
+
+
+//----------------------------------------------------------------------
+// SETUP
+
+void setup() {
+
+  Serial.begin(9600);
+
+  //DMD
+  Disp_init();
+  
+  Disp.setBrightness(20);
+
+  
+}
+
+
+
+
+//----------------------------------------------------------------------
+// LOOP
+
+
+void loop() {
+  
+  Disp.setFont(ElektronMart5x6); 
+  Disp.drawText(1,0, "MEMBUAT JWS");
+  Disp.drawText(1,7, "#01");
+  delay(3000);
+  Disp.clear();
+  
+}
