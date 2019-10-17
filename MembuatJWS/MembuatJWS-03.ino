0415a6baa9b370baef7065014679d76d5843be70
Bonny Useful <bonny.useful@gmail.com>
2019-10-18 00:38:11 +0700

    Add files via upload

---
+++ MembuatJWS/MembuatJWS-03.ino
@@ -0,0 +1,313 @@
+/*
+ * JADWAL WAKTU SHOLAT MENGGUNAKAN NODEMCU ESP8266, LED P10, RTC DS3231, BUZZER
+ * FITUR :  JADWAL SHOLAT 5 WAKTU DAN TANBIH IMSAK, JAM BESAR, TANGGAL, SUHU, ALARAM ADZAN DAN TANBIH IMSAK,
+ *          DAN HITUNG MUNDUR IQOMAH DAN UBAH WAKTU LEWAT WIFI DENGAN BROWSER.
+ * 
+
+Pin on  DMD P10     GPIO      NODEMCU               Pin on  DS3231      NODEMCU                   Pin on  Buzzer       NODEMCU
+        2  A        GPIO16    D0                            SCL         D1 (GPIO 5)                       +            RX (GPIO 3)
+        4  B        GPIO12    D6                            SDA         D2 (GPIO 4)                       -            GND
+        8  CLK      GPIO14    D5                            VCC         3V
+        10 SCK      GPIO0     D3                            GND         GND
+        12 R        GPIO13    D7
+        1  NOE      GPIO15    D8
+        3  GND      GND       GND
+
+Catatan : 
+o Perlu Power Eksternal 5V ke LED P10.
+o Saat Flashing (upload program) cabut sementara pin untuk buzzer.
+
+Project Git
+- https://github.com/busel7/Arduino
+
+Eksternal Library
+- HJS589(DMD3 porting for ESP8266 by Ahmad Herman) < DMD3 by Wardi Utari @ MFH Robotic (info https://www.facebook.com/MFH.Robotic/)) < DMD : https://github.com/freetronics/DMD
+- PrayerTime : https://github.com/asmaklad/Arduino-Prayer-Times
+- RTC DS3231 : https://github.com/Makuna/Rtc
+- ArduinoJson V6 : https://github.com/bblanchon/ArduinoJson
+
+Diskusi Grup Untuk1000Masjid : https://www.facebook.com/groups/761058907424496/
+
+www.grobak.net - www.elektronmart.com
+
+Updated : 27 September 2019
+*/
+
+#include <Wire.h>
+#include <RtcDS3231.h>
+RtcDS3231<TwoWire> Rtc(Wire);
+
+#include <HJS589.h>
+
+#include <fonts/ElektronMart6x8.h>
+#include <fonts/ElektronMart6x16.h>
+#include <fonts/ElektronMart5x6.h>
+#include <fonts/ElektronMartArabic6x16.h>
+#include <fonts/ElektronMartArabic5x6.h>
+
+
+//----------------------------
+// SETUP DMD HJS589
+
+#define DISPLAYS_WIDE 2 
+#define DISPLAYS_HIGH 1
+HJS589 Disp(DISPLAYS_WIDE, DISPLAYS_HIGH);  // Jumlah Panel P10 yang digunakan (KOLOM,BARIS)
+
+
+
+//----------------------------------------------------------------------
+// HJS589 P10 FUNGSI TAMBAHAN UNTUK NODEMCU ESP8266
+
+void ICACHE_RAM_ATTR refresh() { 
+  
+  Disp.refresh();
+  timer0_write(ESP.getCycleCount() + 80000);  
+
+}
+
+void Disp_init() {
+  
+  Disp.start();
+  noInterrupts();
+  timer0_isr_init();
+  timer0_attachInterrupt(refresh);
+  timer0_write(ESP.getCycleCount() + 80000);
+  interrupts();
+  Disp.clear();
+  
+}
+
+
+//SETUP RTC
+//year, month, date, hour, min, sec and week-day(Senin 0 sampai Ahad 6)
+//DateTime dt(2018, 12, 20, 16, 30, 0, 3);
+RtcDateTime now;
+char weekDay[][7] = {"AHAD", "SENIN", "SELASA", "RABU", "KAMIS", "JUM'AT", "SABTU", "AHAD"}; // array hari, dihitung mulai dari senin, hari senin angka nya =0,
+char monthYear[][4] = { "DES", "JAN", "FEB", "MAR", "APR", "MEI", "JUN", "JUL", "AGU", "SEP", "OKT", "NOV", "DES" };
+
+
+// BUZZER
+const int buzzer = 3; // Pin GPIO Buzzer - RX
+
+
+
+//----------------------------------------------------------------------
+// SETUP
+
+void setup() {
+
+  Serial.begin(9600);
+
+  //Buzzer
+  pinMode(buzzer, OUTPUT);
+  digitalWrite(buzzer, LOW);
+  delay(50);
+
+  //RTC D3231
+
+  int rtn = I2C_ClearBus(); // clear the I2C bus first before calling Wire.begin()
+  if (rtn != 0) {
+    Serial.println(F("I2C bus error. Could not clear"));
+    if (rtn == 1) {
+      Serial.println(F("SCL clock line held low"));
+    } else if (rtn == 2) {
+      Serial.println(F("SCL clock line held low by slave clock stretch"));
+    } else if (rtn == 3) {
+      Serial.println(F("SDA data line held low"));
+    }
+  } else { // bus clear, re-enable Wire, now can start Wire Arduino master
+    Wire.begin();
+  }
+  
+  Rtc.Begin();
+
+  if (!Rtc.GetIsRunning()) {
+    
+    Rtc.SetIsRunning(true);
+    
+  }
+  
+  Rtc.Enable32kHzPin(false);
+  Rtc.SetSquareWavePin(DS3231SquareWavePin_ModeNone); 
+  
+  Serial.println("Setup RTC selesai");
+  
+
+  //DMD
+  Disp_init();
+  
+  Disp.setBrightness(20);
+
+  
+}
+
+
+
+
+//----------------------------------------------------------------------
+// LOOP
+
+
+void loop() {
+  
+  Disp.setFont(ElektronMart5x6); 
+  Disp.drawText(1,0, "MEMBUAT JWS");
+  Disp.drawText(1,7, "#03");
+  
+  TampilJamKecil();
+
+  BuzzerPanjang();
+
+  BuzzerPendek();
+
+  //BunyiBeep();
+  
+}
+
+
+void TampilJamKecil() {
+
+  static uint32_t pMJam;
+  uint32_t cM = millis();
+  
+  RtcDateTime now = Rtc.GetDateTime();
+  char jam[9];
+  
+  if (cM - pMJam >= 1000) {
+   
+    pMJam = cM;
+    
+    //JAM
+    sprintf(jam,"%02d:%02d:%02d", now.Hour(), now.Minute(), now.Second());
+    Disp.setFont(ElektronMart5x6);
+    Disp.drawText(20,7, jam);
+        
+  }
+ 
+}
+
+
+
+//----------------------------------------------------------------------
+// BUNYIKAN BEEP BUZZER
+
+void BuzzerPanjang() {
+  
+  digitalWrite(buzzer, HIGH);
+  delay(1000);
+  digitalWrite(buzzer, LOW);
+  delay(1000);
+  digitalWrite(buzzer, HIGH);
+  delay(1000);
+  digitalWrite(buzzer, LOW);
+  delay(3000);
+  
+}
+
+void BuzzerPendek() {
+  
+  digitalWrite(buzzer, HIGH);
+  delay(200);
+  digitalWrite(buzzer, LOW);
+  delay(100);
+  digitalWrite(buzzer, HIGH);
+  delay(200);
+  digitalWrite(buzzer, LOW);
+  delay(3000);
+  
+}
+
+void BunyiBeep() {
+
+  static uint32_t pMBuz;
+  uint32_t cM = millis();
+  static boolean beep;  
+  
+  if (cM - pMBuz >= 200) {
+   
+    pMBuz = cM;
+
+    beep = !beep;    
+        
+  }
+
+  if (beep) {
+
+    digitalWrite(buzzer, HIGH);
+    
+  } else {
+    
+    digitalWrite(buzzer, LOW);
+    
+  }
+ 
+}
+
+
+
+//----------------------------------------------------------------------
+// I2C_ClearBus menghindari gagal baca RTC (nilai 00 atau 165)
+
+int I2C_ClearBus() {
+  
+#if defined(TWCR) && defined(TWEN)
+  TWCR &= ~(_BV(TWEN)); //Disable the Atmel 2-Wire interface so we can control the SDA and SCL pins directly
+#endif
+
+  pinMode(SDA, INPUT_PULLUP); // Make SDA (data) and SCL (clock) pins Inputs with pullup.
+  pinMode(SCL, INPUT_PULLUP);
+
+  delay(2500);  // Wait 2.5 secs. This is strictly only necessary on the first power
+  // up of the DS3231 module to allow it to initialize properly,
+  // but is also assists in reliable programming of FioV3 boards as it gives the
+  // IDE a chance to start uploaded the program
+  // before existing sketch confuses the IDE by sending Serial data.
+
+  boolean SCL_LOW = (digitalRead(SCL) == LOW); // Check is SCL is Low.
+  if (SCL_LOW) { //If it is held low Arduno cannot become the I2C master. 
+    return 1; //I2C bus error. Could not clear SCL clock line held low
+  }
+
+  boolean SDA_LOW = (digitalRead(SDA) == LOW);  // vi. Check SDA input.
+  int clockCount = 20; // > 2x9 clock
+
+  while (SDA_LOW && (clockCount > 0)) { //  vii. If SDA is Low,
+    clockCount--;
+  // Note: I2C bus is open collector so do NOT drive SCL or SDA high.
+    pinMode(SCL, INPUT); // release SCL pullup so that when made output it will be LOW
+    pinMode(SCL, OUTPUT); // then clock SCL Low
+    delayMicroseconds(10); //  for >5uS
+    pinMode(SCL, INPUT); // release SCL LOW
+    pinMode(SCL, INPUT_PULLUP); // turn on pullup resistors again
+    // do not force high as slave may be holding it low for clock stretching.
+    delayMicroseconds(10); //  for >5uS
+    // The >5uS is so that even the slowest I2C devices are handled.
+    SCL_LOW = (digitalRead(SCL) == LOW); // Check if SCL is Low.
+    int counter = 20;
+    while (SCL_LOW && (counter > 0)) {  //  loop waiting for SCL to become High only wait 2sec.
+      counter--;
+      delay(100);
+      SCL_LOW = (digitalRead(SCL) == LOW);
+    }
+    if (SCL_LOW) { // still low after 2 sec error
+      return 2; // I2C bus error. Could not clear. SCL clock line held low by slave clock stretch for >2sec
+    }
+    SDA_LOW = (digitalRead(SDA) == LOW); //   and check SDA input again and loop
+  }
+  if (SDA_LOW) { // still low
+    return 3; // I2C bus error. Could not clear. SDA data line held low
+  }
+
+  // else pull SDA line low for Start or Repeated Start
+  pinMode(SDA, INPUT); // remove pullup.
+  pinMode(SDA, OUTPUT);  // and then make it LOW i.e. send an I2C Start or Repeated start control.
+  // When there is only one I2C master a Start or Repeat Start has the same function as a Stop and clears the bus.
+  /// A Repeat Start is a Start occurring after a Start with no intervening Stop.
+  delayMicroseconds(10); // wait >5uS
+  pinMode(SDA, INPUT); // remove output low
+  pinMode(SDA, INPUT_PULLUP); // and make SDA high i.e. send I2C STOP control.
+  delayMicroseconds(10); // x. wait >5uS
+  pinMode(SDA, INPUT); // and reset pins as tri-state inputs which is the default state on reset
+  pinMode(SCL, INPUT);
+  return 0; // all ok
+}
