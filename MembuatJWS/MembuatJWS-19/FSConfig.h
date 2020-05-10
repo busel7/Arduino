18626b91ee13472f825c97131e99e06dfa25ca2c
Bonny Useful <bonny.useful@gmail.com>
2020-05-10 17:02:42 +0700

    Add files via upload

---
+++ MembuatJWS/MembuatJWS-19/FSConfig.h
@@ -0,0 +1,18 @@
+// ----------------------
+// FS CONFIG
+
+int detikiqmh;
+int menitiqmh;
+
+uint8_t iqmhs = 1; // menit
+uint8_t iqmhd = 1; // menit
+uint8_t iqmha = 1; // menit
+uint8_t iqmhm = 1; // menit
+uint8_t iqmhi = 1; // menit
+
+uint8_t durasiadzan = 1; // Menit
+uint8_t tipealarm;
+
+uint8_t tmputama;
+uint8_t tmpjws;
+uint8_t tmpinfo;
