12660d6414f8302e04b23ecad9921e663091c961
Bonny Useful <bonny.useful@gmail.com>
2020-05-11 20:08:08 +0700

    Add files via upload

---
+++ MembuatJWS/MembuatJWS-21/RTC.h
@@ -0,0 +1,225 @@
+// ------------------------
+// SETUP RTC
+
+RtcDateTime now;
+char namaHari[][7] = {"AHAD", "SENIN", "SELASA", "RABU", "KAMIS", "JUM'AT", "SABTU", "AHAD"}; // array hari, dihitung mulai dari senin, hari senin angka nya =0,
+char namaBulan[][4] = { "DES", "JAN", "FEB", "MAR", "APR", "MEI", "JUN", "JUL", "AGU", "SEP", "OKT", "NOV", "DES" };
+
+uint8_t rJam;
+uint8_t rMen;
+uint8_t rDet;
+uint8_t rTgl;
+uint8_t rHar;
+uint8_t rBul;
+uint16_t rTah;
+int celsius;
+
+
+// PrayerTimes
+double times[sizeof(TimeName)/sizeof(char*)];
+int ihti = 2;
+
+
+//----------------------------------------------------------------------
+// I2C_ClearBus menghindari gagal baca RTC (nilai 00 atau 165)
+
+int I2C_ClearBus() {
+  
+  #if defined(TWCR) && defined(TWEN)
+    TWCR &= ~(_BV(TWEN)); //Disable the Atmel 2-Wire interface so we can control the SDA and SCL pins directly
+  #endif
+
+  pinMode(SDA, INPUT_PULLUP); // Make SDA (data) and SCL (clock) pins Inputs with pullup.
+  pinMode(SCL, INPUT_PULLUP);
+
+  delay(2500);  // Wait 2.5 secs. This is strictly only necessary on the first power
+  // up of the DS3231 module to allow it to initialize properly,
+  // but is also assists in reliable programming of FioV3 boards as it gives the
+  // IDE a chance to start uploaded the program
+  // before existing sketch confuses the IDE by sending Serial data.
+
+  boolean SCL_LOW = (digitalRead(SCL) == LOW); // Check is SCL is Low.
+  if (SCL_LOW) { //If it is held low Arduno cannot become the I2C master. 
+    return 1; //I2C bus error. Could not clear SCL clock line held low
+  }
+
+  boolean SDA_LOW = (digitalRead(SDA) == LOW);  // vi. Check SDA input.
+  int clockCount = 20; // > 2x9 clock
+
+  while (SDA_LOW && (clockCount > 0)) { //  vii. If SDA is Low,
+    clockCount--;
+  // Note: I2C bus is open collector so do NOT drive SCL or SDA high.
+    pinMode(SCL, INPUT); // release SCL pullup so that when made output it will be LOW
+    pinMode(SCL, OUTPUT); // then clock SCL Low
+    delayMicroseconds(10); //  for >5uS
+    pinMode(SCL, INPUT); // release SCL LOW
+    pinMode(SCL, INPUT_PULLUP); // turn on pullup resistors again
+    // do not force high as slave may be holding it low for clock stretching.
+    delayMicroseconds(10); //  for >5uS
+    // The >5uS is so that even the slowest I2C devices are handled.
+    SCL_LOW = (digitalRead(SCL) == LOW); // Check if SCL is Low.
+    int counter = 20;
+    while (SCL_LOW && (counter > 0)) {  //  loop waiting for SCL to become High only wait 2sec.
+      counter--;
+      delay(100);
+      SCL_LOW = (digitalRead(SCL) == LOW);
+    }
+    if (SCL_LOW) { // still low after 2 sec error
+      return 2; // I2C bus error. Could not clear. SCL clock line held low by slave clock stretch for >2sec
+    }
+    SDA_LOW = (digitalRead(SDA) == LOW); //   and check SDA input again and loop
+  }
+  if (SDA_LOW) { // still low
+    return 3; // I2C bus error. Could not clear. SDA data line held low
+  }
+
+  // else pull SDA line low for Start or Repeated Start
+  pinMode(SDA, INPUT); // remove pullup.
+  pinMode(SDA, OUTPUT);  // and then make it LOW i.e. send an I2C Start or Repeated start control.
+  // When there is only one I2C master a Start or Repeat Start has the same function as a Stop and clears the bus.
+  /// A Repeat Start is a Start occurring after a Start with no intervening Stop.
+  delayMicroseconds(10); // wait >5uS
+  pinMode(SDA, INPUT); // remove output low
+  pinMode(SDA, INPUT_PULLUP); // and make SDA high i.e. send I2C STOP control.
+  delayMicroseconds(10); // x. wait >5uS
+  pinMode(SDA, INPUT); // and reset pins as tri-state inputs which is the default state on reset
+  pinMode(SCL, INPUT);
+  return 0; // all ok
+  
+}
+
+
+
+// MULAI RTC DS3231
+
+void mulaiRTC() {
+
+  int rtn = I2C_ClearBus(); // clear the I2C bus first before calling Wire.begin()
+  if (rtn != 0) {
+    Serial.println(F("I2C bus error. Could not clear"));
+    if (rtn == 1) {
+      Serial.println(F("SCL clock line held low"));
+    } else if (rtn == 2) {
+      Serial.println(F("SCL clock line held low by slave clock stretch"));
+    } else if (rtn == 3) {
+      Serial.println(F("SDA data line held low"));
+    }
+  } else { // bus clear, re-enable Wire, now can start Wire Arduino master
+    Wire.begin();
+  }
+  
+  Rtc.Begin();
+
+  if (!Rtc.GetIsRunning()) {
+    
+    Rtc.SetIsRunning(true);
+    
+  }
+  
+  Rtc.Enable32kHzPin(false);
+  Rtc.SetSquareWavePin(DS3231SquareWavePin_ModeNone);
+  
+  Serial.println("Setup RTC selesai");
+  
+}
+
+
+//----------------------------------------------------------------------
+// UPDATE WAKTU
+
+void BacaRTC() {
+  RtcDateTime now = Rtc.GetDateTime();
+  RtcTemperature temp = Rtc.GetTemperature();
+  celsius = temp.AsFloatDegC();
+  rJam = now.Hour();
+  rMen = now.Minute();
+  rDet = now.Second();
+  rTgl = now.Day();
+  rHar = now.DayOfWeek();
+  rBul = now.Month();
+  rTah = now.Year();
+}
+
+
+// PENGHITUNG JADWAL SHOLAT
+
+void JadwalSholat() {
+
+  /*
+    CALCULATION METHOD
+    ------------------
+    Jafari,   // Ithna Ashari
+    Karachi,  // University of Islamic Sciences, Karachi
+    ISNA,     // Islamic Society of North America (ISNA)
+    MWL,      // Muslim World League (MWL)
+    Makkah,   // Umm al-Qura, Makkah
+    Egypt,    // Egyptian General Authority of Survey
+    Custom,   // Custom Setting
+
+    JURISTIC
+    --------
+    Shafii,    // Shafii (standard)
+    Hanafi,    // Hanafi
+
+    ADJUSTING METHOD
+    ----------------
+    None,        // No adjustment
+    MidNight,   // middle of night
+    OneSeventh, // 1/7th of night
+    AngleBased, // angle/60th of night
+
+    TIME IDS
+    --------
+    Fajr,
+    Sunrise,
+    Dhuhr,
+    Asr,
+    Sunset,
+    Maghrib,
+    Isha
+  
+  */
+
+  float latitude = -6.16;
+  float longitude = 106.61;
+  int zonawaktu = 7;
+  
+  set_calc_method(Karachi);
+  set_asr_method(Shafii);
+  set_high_lats_adjust_method(AngleBased);
+  set_fajr_angle(20);
+  set_isha_angle(18);
+
+  get_prayer_times(rTah, rBul, rTgl, latitude, longitude, zonawaktu, times);
+
+}
+
+
+void UpdateWaktu() {
+
+  static long pM;
+  static uint16_t d;
+  
+
+  if (millis() - pM > 1000) {
+    d++;
+    if (d < 2) {
+      BacaRTC();
+      JadwalSholat();
+    } else if (d < 60) {
+      rDet++;
+      if (rDet > 59) {
+        rMen++;
+        rDet=0;
+        if (rMen > 59) {
+          rJam++;
+          rMen = 0;
+        }
+      }
+    } else {
+      d = 0; 
+    }
+    pM = millis();    
+  }
+  
+}
